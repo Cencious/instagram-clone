7e16e89c0b522c2ac14a61a446fa80aec8fb13db
Cencious <user.email>
2022-06-06 05:23:03 +0300

    define the delete notification function , call in request and notification id parameter

--- notification/views.py
+++ notification/views.py
@@ -15,4 +15,7 @@ def ShowNotification(request):
     }
     return render(request, 'notifications/notification.html', context)
 
-
+def DeleteNotification(request, noti_id):
+    user = request.user
+    Notification.objects.filter(id=noti_id, user=user).delete()
+    return redirect('show-notification')
