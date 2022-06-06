582875af96c4dcb74032e1ba46cb60b77521d07d
Cencious <user.email>
2022-06-07 02:35:02 +0300

    add a query for the user if POST, create a form to allow user upload a picture/file

--- post/views.py
+++ post/views.py
@@ -1,5 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from post.models import Post, Tag, Follow, Stream, Likes
+from django.core.paginator import Paginator
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from post.forms import NewPostform
+from authy.models import Profile
 
 # Create your views here.
+@login_required
 def index(request):
-    return render(request, 'index.html') 
+    user = request.user
+    all_users = User.objects.all()
+    folow_status = Follow.objects.filter(following=user, follower=request.user).exists()
+
+    profile =Profile.objects.all()
+    posts = Stream.objects.filter(user=user)
+    group_ids = []
+
+    for post in posts:
+        group_ids.append(post.post_id)
+        
+    post_items = Post.objects.filter(id__in=group_ids).all().order_by('-posted')
+
+    query = request.GET.get('q')
+    if query:
+        users = User.objects.filter(Q(username__icontains=query))
+
+        paginator = Paginator(users, 6)
+        page_number = request.GET.get('page')
+        users_paginator = paginator.get_page(page_number)
+
+    context = {
+        'post_items': post_items,
+        'follow_status': follow_status,
+        'profile': profile,
+        'all_users': all_users,
+        }
+
+    return render(request, 'index.html', context) 
+
+def NewPost(request):
+    user = request.user
+    tags_objs= []
+
+    if request.method == 'POST':
+        form = NewPostform(request.POST, request.FILES)
