7bcb79090d304697079f028fe6bb0900002dd254
Cencious <user.email>
2022-06-06 11:33:20 +0300

    add the follow and follow method for class follow

--- post/models.py
+++ post/models.py
@@ -52,4 +52,8 @@ class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower')
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
 
-    
+    def user_follow(sender,instance, *args, **kwargs):
+        follow = instance
+        sender = follow.follower
+        following = follow.following
+        notify = Notification(senders=sender)
