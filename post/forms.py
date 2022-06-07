c09a30a662540d03b0ed03f50f8ec066de1fd144
Cencious <user.email>
2022-06-08 01:11:07 +0300

    add new post form

--- post/forms.py
+++ post/forms.py
@@ -6,6 +6,7 @@ class NewPostform(forms.ModelForm):
 
     picture = forms.ImageField(required=True)
     caption = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Caption'}), required=True)
+    
     tags = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Tags | Seperate with comma'}), required=True)
 
     class Meta:
