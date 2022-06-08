fb79e6bc0c6ec35270d5d0b4b665d99561a60b76
Cencious <user.email>
2022-06-08 07:53:48 +0300

    add path for user and profiles

--- igprj/urls.py
+++ igprj/urls.py
@@ -15,9 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from authy.views import UserProfile, follow
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('post.urls')),
-     path('notifications/', include('notification.urls')),
+    path('users/', include('authy.urls')),
+    path('notifications/', include('notification.urls')),
+    path('message/', include('directs.urls')),
+   
+
+    # profile
+    path('<username>/', UserProfile, name='profile'),
+    path('<username>/saved/', UserProfile, name='profilefavourite'),
+    path('<username>/follow/<option>/', follow, name='follow'),
+
 ]
+
+# This is used for
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
