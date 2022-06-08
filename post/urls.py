094def7d34869a7feda5d8755a5657b899fc4b04
Cencious <user.email>
2022-06-08 07:56:28 +0300

    add path path app

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from post import views
 
+
 urlpatterns =[
+    
     path('',views.index, name='index'),
+    path('newpost/', views.NewPost, name='newpost'),
+    path('<uuid:post_id>', views.PostDetail, name='post-details'),
+    path('<uuid:post_id>/favourite', views.favourite, name='favourite'),
+    path('tag/<slug:tag_slug>', views.Tags, name='tags'),
+    path('<uuid:post_id>/like', views.like, name='like'),
+
 ]
 
