74d42f5635fc69414f8feb14fa7da795220bfa80
Cencious <user.email>
2022-06-09 23:10:20 +0300

    add tear dwn on tests

--- authy/tests.py
+++ authy/tests.py
@@ -6,12 +6,25 @@ from django.contrib.auth.models import User
 # Create your tests here.
 class TestProfile(TestCase):
     def setUp(self):
-        self.user = User(username='kakan')
+        self.user = User.objects.create(username='kakan')
+        # self.user.save()
+
+        # self.test_profile = Profile(id=1, profile='default.jpg', bio='this is a test profile', user=self.user)
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_new_profile(self):
+        self.assertIsInstance(self.user.profile, Profile)
         self.user.save()
+        self.assertIsInstance(self.user.profile, Profile)
+    
 
-        self.test_profile = Profile(id=1, profile='default.jpg', bio='this is a test profile', user=self.user)
+    # def test_instance(self):
+    #     self.assertTrue(isinstance(self.test_profile, Profile))
+    
 
-    def test_instance(self):
-        self.assertTrue(isinstance(self.test_profile, Profile))
+
+    
 
    
